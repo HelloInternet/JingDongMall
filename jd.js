e24a8f194c18efea28c903aa615fa75091675ede
钱晓斌 <qianxiaobina@yeah.net>
2016-10-15 23:11:35 +0800

    完成40%

--- jd.js
+++ jd.js
@@ -1,3 +1,49 @@
-/**
- * Created by SONY on 2016/10/14.
- */
+var i= 0;
+var timer;
+$(function() {
+    $('.ig').eq(0).show().siblings('a').hide();
+    ShowTimer();
+
+    $('.tab').hover(function () {
+        i = $(this).index();
+        Show();
+        clearInterval(timer);
+    }, function () {
+        ShowTimer();
+    });
+
+    $('.btn1').click(function () {
+        clearInterval(timer);
+        if (i == 0) {
+            i = 6;
+        }
+        i--;
+        Show();
+        ShowTimer();
+    });
+
+    $('.btn2').click(function () {
+        clearInterval(timer);
+        if (i == 5) {
+            i = -1;
+        }
+        i++;
+        Show();
+        ShowTimer();
+    });
+});
+
+function Show(){
+    $('.ig').eq(i).fadeIn(300).siblings('a').fadeOut(300);
+    $('.tab').eq(i).addClass('bg').siblings().removeClass('bg');
+}
+
+function ShowTimer(){
+    timer = setInterval(function(){
+        i++;
+        if(i==6){
+            i=0;
+        }
+        Show();
+    },4000);
+}
